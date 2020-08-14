0db363f9265c04a7c2de163bc9e3c1a3d04332a6
wjdskagjs1 <wjdskagjs1@naver.com>
2020-08-14 23:55:08 +0900

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # smpeopleCrawler
+
+https://discord.com/api/oauth2/authorize?client_id=743704510043521024&permissions=18432&scope=bot
+
+smpeople 프로그래밍 게시판(https://smpeople.net/hobby_program) 새 글 알림 봇입니다.
+
+한시간 주기로 https://smpeople.net/hobby_program 의 최신 게시물을 긁어와서 디코의 "프로그래밍-게시판"라는 이름의 채팅 채널이 있어야 링크를 올려줍니다.
+
+>도움말
+도움말을 출력합니다.
+
+>갱신
+주기를 무시하고 최신 게시글을 긁어옵니다.
+
+
+자스맨 모여라 디코
+https://discord.gg/fbD4GSS
--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('./config.json'));
+const help = fs.readFileSync('./README.md');
 
 const axios = require("axios");
 const cheerio = require("cheerio");
-const log = console.log;
-let before = 75;
 
 const setting = {
     interval: 60*60*1000,
     boardType: 'hobby_program',
-    channelName: '프로그래밍-게시판'
+    channelName: '프로그래밍-게시판',
+    guild:{},
 };
 
 const getHtml = async () => {
@@ -21,29 +21,6 @@ const getHtml = async () => {
     console.error(error);
   }
 };
-const reload = ()=>{
-    getHtml()
-    .then(html => {
-      let ulList = [];
-      const $ = cheerio.load(html.data);
-      const $bodyList = $('tbody').children('tr');
-      
-      $bodyList.each(function(i, elem) {
-          if(before < parseInt($(elem).find('td.no').text())){
-              ulList.push($(elem).find('td.title a').attr('href'));
-              before = before + 1;
-          }
-      });
-      return ulList;
-    })
-    .then(res => {
-        log("리스트:",res," 이전 번호:", before);
-        res.forEach((element)=>{
-            const channel = client.channels.cache.find(c => c.name === setting.channelName);
-            channel.send(`https://smpeople.net${element}`);
-        });
-    });
-}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -53,21 +30,41 @@ client.setInterval(()=>{
 }, setting.interval);//end client
 
 client.on('message', async (msg) => {
-    if(msg.content === "/갱신"){
+
+    const guildName = msg.guild.name;
+    const reload = ()=>{
+        getHtml()
+        .then(html => {
+          let ulList = [];
+          const $ = cheerio.load(html.data);
+          const $bodyList = $('tbody').children('tr');
+
+          $bodyList.each(function(i, elem) {
+            if(setting.guild[guildName] === undefined){
+                setting.guild[guildName] = 0;
+            }
+              if(setting.guild[guildName] < parseInt($(elem).find('td.no').text())){
+                  ulList.push($(elem).find('td.title a').attr('href'));
+                  setting.guild[guildName] = parseInt($(elem).find('td.no').text());
+              }
+          });
+          return ulList.reverse();
+        })
+        .then(res => {
+            res.forEach((element)=>{
+                const channel = client.channels.cache.find(c => c.name === setting.channelName);
+                channel.send(`https://smpeople.net${element}`);
+                
+            });
+        });
+    }
+    if(msg.content === ">도움말"){
+        let str = "\`\`\`"+help+"\`\`\`";
+        msg.channel.send(str);
+    }
+    if(msg.content === ">갱신"){
         await reload();
         msg.channel.send(`${setting.channelName}이 갱신되었습니다.`);
-    }
-    if(msg.content.startsWith("/interval")){
-        const arr = msg.content.split(' ');
-        setting.interval = eval(arr[1]);
-    }
-    if(msg.content.startsWith("/boardType")){
-        const arr = msg.content.split(' ');
-        setting.boardType = arr[1];
-    }
-    if(msg.content.startsWith("/channelName")){
-        const arr = msg.content.split(' ');
-        setting.channelName = arr[1];
     }
 });
 
